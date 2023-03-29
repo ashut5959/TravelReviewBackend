2a0c34d5defdd0b4f71f1327b66b1ab57e70ad4f
ashut5959 <ashutoshpal59@gmail.com>
2023-03-29 23:34:23 +0530

    (IMports)

--- files/import-dev-data.js
+++ files/import-dev-data.js
@@ -1,48 +1,53 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv'); 
+const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
-const fs = require('fs')
-const Tour = require('../models/tourModel.js')
+const fs = require('fs');
+const Tour = require('../models/tourModel.js');
+const User = require('../models/userModel.js');
+const Review = require('../models/reviewModel.js');
 mongoose.set('strictQuery', false);
 
 // console.log(process.env)
 console.log(process.env.DATABASE);
 const DB = process.env.DATABASE.replace(
-    '<password>',
-    process.env.DATABASE_PASSWORD
-  );
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
 
 mongoose.connect(DB).then(() => {
   // console.log(con.connections);
   console.log('Db is Connected');
 });
 
-
 //READ JSON FILE
-const tours = JSON.parse(fs.readFileSync('./tours.json','utf-8'));
+const tours = JSON.parse(fs.readFileSync('./tours.json', 'utf-8'));
+const users = JSON.parse(fs.readFileSync('./users.json', 'utf-8'));
+const revoews = JSON.parse(fs.readFileSync('./tours.json', 'utf-8'));
 
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(revoews);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const importData  = async () => {
-    try {
-        await Tour.create(tours)
-    } catch (error) {
-        console.log(error)
-    }
-}
+const deleteData = async () => {
+  try {
+      await Tour.deleteMany();
+      await User.deleteMany();
+      await Review.deleteMany();
+      console.log('Data successfully deleted!');
+  } catch (error) {
+    console.log(error);
+  }
+  process.exit();
+};
 
-const deleteData = async() =>{
-    try {
-        await Tour.deleteMany();
-        console.log('Data successfully deleted!')
-    } catch (error) {
-        console.log(error)
-    }
-    process.exit();
-}
-
-if(process.argv[2] === '--import') {
-    importData();
+if (process.argv[2] === '--import') {
+  importData();
 } else if (process.argv[2] === '--delete') {
-    deleteData()
+  deleteData();
 }
-
